6092e3f048835c223c2985c911e41f5df9255a80
Pavel V <vorona.pavel.nickolaevich@gmail.com>
2019-03-14 19:38:44 +0300

    Make chart smaller

--- create-config.js
+++ create-config.js
@@ -2,30 +2,32 @@ function createChartConfig (chartData) {
   const graphNames = chartData.columns.map(column => column[0]).filter(graphName => chartData.types[graphName] === 'line')
 
   const canvases = createCanvases(graphNames, {
-    style: 'width: 1360px; height: 480px',
-    width: 2720,
-    height: 960,
+    style: 'width: 768px; height: 400',
+    width: 1536,
+    height: 600,
     className: 'layer',
   })
   const canvasesContainer = createElement('div', {
     className: 'layers-container',
-    style: 'width: 1000px; height: 480px; position: relative;',
+    style: 'width: 768px; height: 300px; position: relative;',
   }, Object.values(canvases))
   const frameCanvases = createCanvases(graphNames, {
-    style: 'width: 1360px; height: 50px',
-    width: 2720,
+    style: 'width: 768px; height: 50px',
+    width: 1536,
     height: 100,
     className: 'layer',
   })
   const frameCanvasContainer = createElement('div', {
-    style: 'width: 1360px; height: 50px',
+    style: 'width: 768px; height: 50px',
   }, Object.values(frameCanvases))
   const backgroundLeft = createElement('div', { className: 'frame__background-left' })
   const backgroundRight = createElement('div', { className: 'frame__background-right' })
   const resizerLeft = createElement('div', { className: 'frame__resizer frame__resizer-left' })
   const resizerRight = createElement('div', { className: 'frame__resizer frame__resizer-right' })
   const framer = createElement('div', { className: 'framer' }, [resizerLeft, resizerRight])
-  const frameContainer = createElement('div', { className: 'frame__container' }, [frameCanvasContainer, backgroundLeft, backgroundRight, framer])
+  const frameContainer = createElement('div', {
+    style: 'margin-top: 40px; position: relative; width: 768px;'
+  }, [frameCanvasContainer, backgroundLeft, backgroundRight, framer])
   const inputs = graphNames.reduce((buttons, graphName) => ({
     ...buttons,
     [graphName]: createElement('input', { checked: true, type: 'checkbox', className: 'button' }),
