55d3b9743cc877f692b26f5e5b7a96f39da9b4c1
Pavel V <vorona.pavel.nickolaevich@gmail.com>
2019-03-17 15:25:59 +0300

    Log scale

--- x-axis.js
+++ x-axis.js
@@ -1,5 +1,6 @@
 const MONTHS = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 const LEGEND_ITEM_CLASS = 'legend-item-value'
+const LEGEND_ITEM_HIDDEN_CLASS = 'legend-item-value--hidden'
 
 function XAxis ({
   domain,
@@ -7,7 +8,7 @@ function XAxis ({
   viewBox,
 }) {
   const containerElement = document.createElement('div')
-  containerElement.style = 'overflow:hidden;max-width: 768px;'
+  containerElement.style = 'overflow:hidden;max-width: 768px;padding: 5px 0 15px;'
   const shiftingContainer = document.createElement('div')
   shiftingContainer.classList.add('shifting-container')
   containerElement.appendChild(shiftingContainer)
@@ -25,22 +26,20 @@ function XAxis ({
   reconcile()
 
   function reconcile () {
+    const stepMiltiplier = calculateMultiplier(viewBox.endIndex - viewBox.startIndex)
     const xScale = (viewBox.endIndex - viewBox.startIndex) / (domain.length - 1)
-    // console.log(viewBox)
-    shiftingContainer.style.transform = `translateX(${-1 / xScale * 768 * viewBox.startIndex / (domain.length - 1)}px)`
-    let previousOffset = -Infinity
-    // console.log(points)
-    // console.log(xScale)
+    const shift = -1 / xScale * 768 * viewBox.startIndex / (domain.length - 1)
+    shiftingContainer.style.transform = `translateX(${shift}px)`
     for (let i = 0; i < points.length; i++) {
       const xValueElement = legendValues[i]
       const offset = points[i].x / xScale
       xValueElement.style.transform = `translateX(${offset}px)`
-      if (offset - previousOffset < 150) {
-        xValueElement.hidden = true
-      } else {
-        previousOffset = offset
-        xValueElement.hidden = false
-      }
+      xValueElement.classList.toggle(
+        LEGEND_ITEM_HIDDEN_CLASS,
+        i % Math.pow(2, stepMiltiplier)
+        || (offset < -1 * shift)
+        || (xValueElement.offsetWidth + offset + shift > 768)
+      )
     }
   }
 
@@ -55,5 +54,21 @@ function XAxis ({
   function getLabelText (timestamp) {
     const date = new Date(timestamp)
     return `${MONTHS[date.getMonth()]} ${date.getDate()}`
+  }
+
+  // Not smart enough to find analytic representation for this function
+  function calculateMultiplier (size) {
+      if      (size < Math.pow(2, 3)) return 0
+      else if (size < Math.pow(2, 4)) return 1
+      else if (size < Math.pow(2, 5)) return 2
+      else if (size < Math.pow(2, 6)) return 3
+      else if (size < Math.pow(2, 7)) return 4
+      else if (size < Math.pow(2, 8)) return 5
+      else if (size < Math.pow(2, 9)) return 6
+      else if (size < Math.pow(2, 10)) return 7
+      else if (size < Math.pow(2, 11)) return 8
+      else if (size < Math.pow(2, 12)) return 9
+      else if (size < Math.pow(2, 13)) return 10
+
   }
 }
