8f1b8a0f7e80c95443183a7c3be66ae320e78dce
Pavel V <vorona.pavel.nickolaevich@gmail.com>
2019-03-24 16:11:08 +0300

    Finalize bundling, gzip assets. JS - 4.6kb, CSS - 1.2kb

--- components/create-config.js
+++ components/create-config.js
@@ -1,10 +1,9 @@
 import { ceil, max } from '../util'
 const LINE_WIDTH = 2
 const OVERVIEW_LINE_WIDTH = 1
-// const CANVAS_WIDTH = 768
-// const CANVAS_HEIGHT = 300
-const CANVAS_WIDTH = 350
-const CANVAS_HEIGHT = 150
+// Change here to test mobile screens
+const CANVAS_WIDTH = 768
+const CANVAS_HEIGHT = 300
 const OVERVIEW_CANVAS_HEIGHT = 50
 const OVERVIEW_CANVAS_WIDTH = CANVAS_WIDTH
 
