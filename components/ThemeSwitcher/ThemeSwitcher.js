d98aa326ee13683dc718b299770974338e72689a
Pavel Vorona <pvorona@workfusion.com>
2019-04-02 13:35:47 +0300

    Extract createDOM in theme switcher, add will-change contents

--- components/ThemeSwitcher/ThemeSwitcher.js
+++ components/ThemeSwitcher/ThemeSwitcher.js
@@ -8,16 +8,21 @@ const ELEMENT_CLASS_NAME = 'theme-switcher'
 
 export function ThemeSwitcher (initialTheme) {
   let theme = initialTheme
+  const button = createDOM()
 
-  const button = document.createElement('button')
-  button.innerText = LABELS[theme]
-  button.classList.add(ELEMENT_CLASS_NAME)
-  button.addEventListener('click', function () {
+  button.onclick = () => {
     document.body.classList.remove(THEME_CLASS_NAMES[theme])
     theme = NEXT_THEME[theme]
     button.innerText = LABELS[theme]
     document.body.classList.add(THEME_CLASS_NAMES[theme])
-  })
+  }
 
   return button
+
+  function createDOM () {
+    const button = document.createElement('button')
+    button.innerText = LABELS[theme]
+    button.classList.add(ELEMENT_CLASS_NAME)
+    return button
+  }
 }
