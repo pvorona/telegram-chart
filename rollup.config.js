aa3cda5968f25da8316938f60343b4dea5d4d3fc
pvorona <vorona.pavel.nickolaevich@gmail.com>
2022-01-28 19:57:28 +0300

    Remove `rollup-plugin-filesize`

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import css from 'rollup-plugin-css-porter'
 import resolve from 'rollup-plugin-node-resolve'
-// import filesize from 'rollup-plugin-filesize'
 import typescript from 'rollup-plugin-typescript2'
 import json from '@rollup/plugin-json'
 
@@ -14,7 +13,6 @@ module.exports = {
     json(),
     css({ minified: false }),
     resolve(),
-    // filesize({ showBrotliSize: true }),
     typescript(),
   ],
 }
