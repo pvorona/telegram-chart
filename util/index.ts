699dd67d3b9e55f98c667490c78f7f824183ddbd
pvorona <vorona.pavel.nickolaevich@gmail.com>
2022-02-05 22:50:05 +0300

    wip

--- util/index.ts
+++ util/index.ts
@@ -16,6 +16,6 @@ export * from "./getTooltipDateText";
 export * from "./areNumbersClose";
 export * from "./beautifyNumber";
 export * from "./createMinMaxView";
-export * from "./toBitMap";
+export * from "./toBitMapSize";
 export * from "./interpolatePoint";
 export * from "./createCache";
